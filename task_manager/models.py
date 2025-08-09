ec637ab4a8d84b04953aef96d91a834fb72361f8
Yakiv Seleznov <yakov.seleznyov@gmail.com>
2025-08-09 17:56:06 +0200

    feat: add Position model

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+
+class Position(models.Model):
+    name = models.CharField(max_length=63, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def validate_unique(self, exclude=None):
+        super().validate_unique(exclude=exclude)
+        if (
+            Position.objects.filter(name__iexact=self.name)
+            .exclude(pk=self.pk)
+            .exists()
+        ):
+            raise ValidationError(
+                {"name": "This name must be unique (case-insensitive)."}
+            )
+
+    def __str__(self):
+        return self.name
